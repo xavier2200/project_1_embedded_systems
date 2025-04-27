44f2b6cec9f3c7054c91467e860702578922452f
xavier <xbs20@hotmail.com>
2025-04-27 21:33:30 +0000

    modify to only add pytorch using pip

--- python_test/yolo_dependencies.sh
+++ python_test/yolo_dependencies.sh
@@ -5,19 +5,19 @@
 echo "Installing PyTorch with CPU-only version..."
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 
-echo "Installing matplotlib..."
-pip3 install matplotlib
+#echo "Installing matplotlib..."
+#pip3 install matplotlib
 
-echo "Installing tqdm..."
-pip3 install tqdm
+#echo "Installing tqdm..."
+#pip3 install tqdm
 
-echo "Installing pyyaml..."
-pip3 install pyyaml
+#echo "Installing pyyaml..."
+#pip3 install pyyaml
 
-echo "Installing requests..."
-pip3 install requests
+#echo "Installing requests..."
+#pip3 install requests
 
-echo "Installing psutil..."
-pip3 install psutil
+#echo "Installing psutil..."
+#pip3 install psutil
 
 echo "All packages installed successfully!"
